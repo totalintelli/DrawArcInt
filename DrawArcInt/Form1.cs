232b9f4cc32ba593d361406f9cc50f0d96361ba0
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-22 15:51:21 +0900

    first commit

---
+++ DrawArcInt/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawArcInt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Create pen.
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            // Create coordinates of rectangle to bound ellipse.
+            int x = 0;
+            int y = 0;
+            int width = 100;
+            int height = 200;
+
+            // Create start and sweep angles on ellipse.
+            int startAngle = 45;
+            int sweepAngle = 270;
+
+            // Draw arc to screen.
+            e.Graphics.DrawArc(blackPen, x, y, width, height, startAngle, sweepAngle);
+        }
+    }
+}
